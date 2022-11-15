254df6bf87466c65f13563de5c2c58e6a17dc27f
UtkarshaAgrawal <111394180+UtkarshaAgrawal@users.noreply.github.com>
2022-11-15 13:36:09 -0500

    Set up CI/CD with Azure Pipelines (#3)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Adding Junit report
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * resolving conflicts
    
    * resolving pipeline error
    
    * fixing allure report generation path
    
    * resolving allure report path
    
    * correcting allure report path
    
    * allure path
    
    * making temporary comments
    
    * removing temporary allure changes
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * removing retries
    
    * Allure report oytput folder update
    
    * removing multiple reports
    
    * removing multiple reports
    
    * removing multiple reports
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * updating config file
    
    * Search results verified
    
    Co-authored-by: Utkarsha Agrawal <utkarsha@Utkarshas-MacBook-Air.local>

--- playwright.config.js
+++ playwright.config.js
@@ -28,11 +28,23 @@ const config = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
-  retries: 2,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["list"], ["html"], ["allure-playwright"]],
+  reporter: [
+    ["list"],
+    ["html", { open: "never" }],
+    ["junit", { outputFile: "results.xml" }],
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        outputFolder: "allure-results",
+        suiteTitle: true,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
